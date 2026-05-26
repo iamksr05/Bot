85126528c45d27d51a1de5fea3c3632778d23b97
Karan Ram <Karanram73005@gmail.com>
2026-05-26 19:48:05 +0530

    updated nickname.js

--- commands/nickname.js
+++ commands/nickname.js
@@ -51,7 +51,7 @@ module.exports = {
 
         if (!mentionedMember) return message.channel.send('The member is not in the server.');
         if (mentionedMember.user.id === '747042752415531021') return message.channel.send("You cannot change my developer's nickname.");
-        if (message.author.id !== '747042752415531021' && mentionedMember.user.id === message.guild.ownerID) return message.channel.send("You cannot change the server owner's nickname.");
+        // if (message.author.id !== '747042752415531021' && mentionedMember.user.id === message.guild.ownerID) return message.channel.send("You cannot change the server owner's nickname.");
         if (mentionedMember.user.id === '827874200394924082' && message.author.id !== message.guild.ownerID) return message.channel.send('You cannot change my nickname.');
 
         const nickName = args.slice(1).join(" ");
