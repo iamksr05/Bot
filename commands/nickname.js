eb83a0c024fdf02f9c99bcdb9abf5f73b9618d3f
Karan Ram <Karanram73005@gmail.com>
2026-05-26 19:52:25 +0530

    updated nickname.js

--- commands/nickname.js
+++ commands/nickname.js
@@ -14,7 +14,7 @@
 //         const mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 //         const nickName = args.slice(1).join(" ");
 //         const lastName = mentionedMember.user.username;
-        
+
 //         if (!args[0]) return message.channel.send('You must specify a member to change nickname.\nCommand Format: \`!nickname @user [nickname]\`');
 //         if (!mentionedMember) return message.channel.send('The member mentioned is not in the server.');
 //         if (!nickName) return message.channel.send('You must state a nickname for the member');
@@ -43,6 +43,8 @@ module.exports = {
     description: "This command sets nickname",
 
     async execute(message, args, cmd, client, Discord) {
+        const { member } = message;
+        
 
         if (!message.guild.me.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("I require `MANAGE_NICKNAMES` permission to use this command!");
 
@@ -71,7 +73,9 @@ module.exports = {
 
         } catch (err) {
             console.log(err);
+            member.guild.channels.cache.get('835486658552004608').send(err);
             message.channel.send("There was an error changing that member's nickname.");
+
         }
     }
 }
