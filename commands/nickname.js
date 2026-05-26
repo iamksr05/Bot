1e148d692a684982b4123ffc51286a11cdaa8f9a
Karan Ram <Karanram73005@gmail.com>
2026-05-26 19:47:06 +0530

    updated nickname.js

--- commands/nickname.js
+++ commands/nickname.js
@@ -44,35 +44,19 @@ module.exports = {
 
     async execute(message, args, cmd, client, Discord) {
 
-        if (!message.guild.me.hasPermission("MANAGE_NICKNAMES")) {
-            return message.channel.send(
-                "I require `MANAGE_NICKNAMES` permission to use this command!"
-            );
-        }
+        if (!message.guild.me.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("I require `MANAGE_NICKNAMES` permission to use this command!");
 
         if (!args[0]) return message.channel.send('You must specify a member.\nFormat: `!nickname @user [nickname]`');
-
         const mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
         if (!mentionedMember) return message.channel.send('The member is not in the server.');
         if (mentionedMember.user.id === '747042752415531021') return message.channel.send("You cannot change my developer's nickname.");
-        if (mentionedMember.user.id === message.guild.ownerID && message.author.id !== '747042752415531021') return message.channel.send("You cannot change the server owner's nickname.");
+        if (message.author.id !== '747042752415531021' && mentionedMember.user.id === message.guild.ownerID) return message.channel.send("You cannot change the server owner's nickname.");
         if (mentionedMember.user.id === '827874200394924082' && message.author.id !== message.guild.ownerID) return message.channel.send('You cannot change my nickname.');
 
         const nickName = args.slice(1).join(" ");
-
-        if (!nickName) {
-            return message.channel.send(
-                'You must provide a nickname.'
-            );
-        }
-
-        if (nickName.length > 32) {
-            return message.channel.send(
-                'Nickname is too long.'
-            );
-        }
-
+        if (!nickName) return message.channel.send('You must provide a nickname.');
+        if (nickName.length > 32) return message.channel.send('Nickname is too long.');
         const lastName = mentionedMember.user.username;
 
         const embed = new Discord.MessageEmbed()
@@ -87,10 +71,7 @@ module.exports = {
 
         } catch (err) {
             console.log(err);
-
-            message.channel.send(
-                "There was an error changing that member's nickname."
-            );
+            message.channel.send("There was an error changing that member's nickname.");
         }
     }
 }
