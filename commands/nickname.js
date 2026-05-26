cce5a282a5b84ccf17dd4d6fcbf7f0a5fefff16d
Karan Ram <Karanram73005@gmail.com>
2026-05-26 20:16:04 +0530

    updated nickname.js

--- commands/nickname.js
+++ commands/nickname.js
@@ -14,7 +14,7 @@
 //         const mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 //         const nickName = args.slice(1).join(" ");
 //         const lastName = mentionedMember.user.username;
-
+        
 //         if (!args[0]) return message.channel.send('You must specify a member to change nickname.\nCommand Format: \`!nickname @user [nickname]\`');
 //         if (!mentionedMember) return message.channel.send('The member mentioned is not in the server.');
 //         if (!nickName) return message.channel.send('You must state a nickname for the member');
@@ -43,8 +43,6 @@ module.exports = {
     description: "This command sets nickname",
 
     async execute(message, args, cmd, client, Discord) {
-        const { member } = message;
-        
 
         if (!message.guild.me.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("I require `MANAGE_NICKNAMES` permission to use this command!");
 
@@ -52,8 +50,8 @@ module.exports = {
         const mentionedMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
         if (!mentionedMember) return message.channel.send('The member is not in the server.');
-        if (mentionedMember.user.id === '747042752415531021' && message.author.id !== '747042752415531021') return message.channel.send("You cannot change my developer's nickname.");
-        // if (message.author.id !== '747042752415531021' && mentionedMember.user.id === message.guild.ownerID) return message.channel.send("You cannot change the server owner's nickname.");
+        if (mentionedMember.user.id === '747042752415531021') return message.channel.send("You cannot change my developer's nickname.");
+        if (message.author.id !== '747042752415531021' && mentionedMember.user.id === message.guild.ownerID) return message.channel.send("You cannot change the server owner's nickname.");
         if (mentionedMember.user.id === '827874200394924082' && message.author.id !== message.guild.ownerID) return message.channel.send('You cannot change my nickname.');
 
         const nickName = args.slice(1).join(" ");
@@ -73,10 +71,7 @@ module.exports = {
 
         } catch (err) {
             console.log(err);
-            member.guild.channels.cache.get('835486658552004608').send(err);
-            console.log(message);
             message.channel.send("There was an error changing that member's nickname.");
-
         }
     }
 }
